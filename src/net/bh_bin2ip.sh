6bdcf337dbc585b48a3b91ea8593a271bb815f2b
sorpigal <sorpigal@gmail.com>
2023-11-19 01:15:51 -0500

    bh_bin2ip: that's not how variable declaration works.

--- src/net/bh_bin2ip.sh
+++ src/net/bh_bin2ip.sh
@@ -1,8 +1,7 @@
 bh_bin2ip() {
 	(( $# < 1 )) && return 1
 
-	local a=b=c=d=
-	
-	IFS=. read a b c d <<< "$1"								
-	echo $((2#$a)).$((2#$b)).$((2#$c)).$((2#$d))
+	local a b c d
+	IFS=. read a b c d <<< "$1"
+	printf %s\\n $((2#$a)).$((2#$b)).$((2#$c)).$((2#$d))
 }
