3ec563f450e497259cf400be504e9d74d070e202
sorpigal <sorpigal@gmail.com>
2023-11-19 11:26:24 -0500

    internal: aliases are bad.

--- src/internal/bh_bootstrap.sh
+++ src/internal/bh_bootstrap.sh
@@ -4,20 +4,49 @@ BASHACKS_OS=$(uname -s)
 BASHACKS_WGET_USER_AGENT="Mozilla/5.0 (Windows NT 6.1; WOW64; rev:28.0) Gecko'20100101 Firefox/28.0"
 
 if [[ $BASHACKS_OS = 'Darwin' ]]; then
-	alias bh_cmd_md5='md5 -r'
-	alias bh_cmd_sha1='shasum -a 1'
-	alias bh_cmd_sha256='shasum -a 256'
-	alias bh_cmd_sha512='shasum -a 512'
-	alias bh_cmd_disasm='objdump -d -x86-asm-syntax=intel'
-	alias bh_cmd_sed_ext='sed -E'
+	bh_cmd_md5 () {
+		command md5 -r "$@"
+	}
+	bh_cmd_sha1 () {
+		command shasum -a 1 "$@"
+	}
+	bh_cmd_sha256 () {
+		command shasum -a 256 "$@"
+	}
+	bh_cmd_sha512 () {
+		command shasum -a 512 "$@"
+	}
+	bh_cmd_disasm () {
+		command objdump -d -x86-asm-syntax=intel "$@"
+	}
+	bh_cmd_sed_ext () {
+		command sed -E "$@"
+	}
 else # Linux
-	alias bh_cmd_md5='md5sum'
-	alias bh_cmd_sha1='sha1sum'
-	alias bh_cmd_sha256='sha256sum'
-	alias bh_cmd_sha512='sha512sum'
-	alias bh_cmd_disasm='objdump -dM intel'
-	alias bh_cmd_sed_ext='sed -r'
+	bh_cmd_md5 () {
+		command md5sum "$@"
+	}
+	bh_cmd_sha1 () {
+		command sha1sum "$@"
+	}
+	bh_cmd_sha256 () {
+		command sha256sum "$@"
+	}
+	bh_cmd_sha512 () {
+		command sha512sum "$@"
+	}
+	bh_cmd_disasm () {
+		command objdump -d1 intel "$@"
+	}
+	bh_cmd_sed_ext () {
+		command sed -r "$@"
+	}
 fi
 
-alias bh_cmd_wget="wget -T10 -U \"$BASHACKS_WGET_USER_AGENT\""
-alias bh_rot13='bh_rot 13'
+bh_cmd_wget () {
+	command wget -T10 -U \"$BASHACKS_WGET_USER_AGENT\"" "$@"
+}
+
+bh_rot13 () {
+	bh_rot 13 "$@"
+}
