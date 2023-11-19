2db1490b4f554695a482eb439fb8e0956d2cb730
sorpigal <sorpigal@gmail.com>
2023-11-19 11:37:21 -0500

    bh_hashes: remove some of the stupid

--- src/filesystem/bh_hashes.sh
+++ src/filesystem/bh_hashes.sh
@@ -1,11 +1,13 @@
 bh_hashes() {
 	(( $# < 1 )) && return 1
 
-	local i
-
-	for i in $*; do
-		echo $(bh_cmd_md5 "$i")
-		echo $(bh_cmd_sha1 "$i")
-		echo $(bh_cmd_sha256 "$i")
-	done | tr -s ' ' ' '
+	local i words
+	for i; do
+		IFS=' ' read -ra words < (
+			bh_cmd_md5 "$i"
+			bh_cmd_sha1 "$i"
+			bh_cmd_sha256 "$i"
+		)
+		printf '%s\n' "$words[@]"
+	done
 }
