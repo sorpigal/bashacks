5d33f95c6aa345294bba3dee609f0953f46a58ca
sorpigal <sorpigal@gmail.com>
2023-11-19 11:42:34 -0500

    bh_md5rename: file name safety, avoid cut

--- src/filesystem/bh_md5rename.sh
+++ src/filesystem/bh_md5rename.sh
@@ -1,11 +1,9 @@
 bh_md5rename() {
 	(( $# < 1 )) && return 1
 
-	local md5_hash
-	local i
-
-	for i in $*; do
-		md5_hash=$(bh_cmd_md5 "$i" | cut -d" " -f1)
-		[[ $md5_hash ]] && mv "$i" $(dirname "$i")/$md5_hash
+	local i md5_hash
+	for i; do
+		IFS=' ' read -r md5_hash _ < <(bh_cmd_md5 "$i")
+		[[ -n $md5_hash ]] && mv "$i" "$(dirname "$i")/$md5_hash"
 	done
 }
