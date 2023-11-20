8ecc9405e6a47a1971963aed36caabbd59ed51d7
sorpigal <sorpigal@gmail.com>
2023-11-19 20:59:28 -0500

    bh_rotall: avoid using echo

--- src/crypto/bh_rotall.sh
+++ src/crypto/bh_rotall.sh
@@ -1,9 +1,8 @@
-bh_rotall() {
+bh_rotall () {
 	(( $# < 1 )) && return 1
 
 	local i
-
 	for i in {1..25}; do
-		echo "ROT$i $(bh_rot $i "$1")"
+		printf 'ROT%d %s\n' "$i" "$(bh_rot "$i" "$1")"
 	done
 }
