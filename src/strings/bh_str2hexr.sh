f32170f121724176d260fa38935df11160a71e83
sorpigal <sorpigal@gmail.com>
2023-12-01 13:10:55 -0500

    bh_str2hexr: the same rewrite used for bh_str2hex, but in reverse

--- src/strings/bh_str2hexr.sh
+++ src/strings/bh_str2hexr.sh
@@ -1,12 +1,48 @@
-bh_str2hexr() {
-    (( $# < 1 )) && return 1
+bh_str2hexr () {
+	(( $# )) || return 1
 
-    case "$1" in
-	    "-x" | "-0x" | "-c")
-	        bh_str2hex $1 "$(echo "$2" | rev)"
-		;;
-        *)
-		    bh_str2hex "$(echo "$1" | rev)"
-	    ;;
-	esac
+	local i c o h l m s
+
+	while (( $# )); do
+		case $1 in
+			-x)
+				m=x
+			;;
+			-0x)
+				m=0x
+			;;
+			-c)
+				m=c
+			;;
+			*)
+				l=()
+				for ((i=${#1}-1;i>=0;i--)); do
+					c=${1:i:1}
+					printf -v o %d "${c/#/\'}"
+					printf -v h %x "$o"
+					l+=("$h")
+				done
+
+				case $m in
+					x)
+						printf '\\x%s' "${l[@]}"
+						printf '\n'
+					;;
+					0x)
+						l=("${l[@]/#/0x}")
+						printf '%s\n' "${l[*]}"
+					;;
+					c)
+						l=("${l[@]/#/0x}")
+						printf -v s ', %s' "${l[@]}"
+						printf '{ %s }\n' "${s:2}"
+					;;
+					*)
+						printf '%s\n' "${l[*]}"
+					;;
+				esac
+			;;
+		esac
+		shift
+	done
 }
