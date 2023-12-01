876bd4fdcf33f0d39dd7a837cb15802fc838606d
sorpigal <sorpigal@gmail.com>
2023-12-01 12:34:21 -0500

    str2hex: stylistic cleanup
    A variable expanded as a parameter to "case" is implicitly expanded as if
    double-quoted. Although the double quotes do no harm, and when in doubt you
    should always prefer to double quote expansions, in this scenario it safe to
    leave it unquoted and I have made that change here/
    
    I have also un-quoted the pattern values in the case statement. Quoting them
    does make them literal, but it is also not required when meta characters are
    not in use, which they are not in this case.
    
    There was some inconsistent indentation on the default cause which has been
    normalized to match the other cases.
    
    Finally, I have moved the position of the ;; that closes a case condition to
    align with the match pattern. I prefer this style as to me it forms a kind o
    a block. I consider this:
    
    	case $1 in
    		foo)
    			printf '%s\n' "$1"
    		;;
    	esac
    
    To be equivalent to this C-style switch:
    
    	switch (argv[1]) {
    		case "foo": {
    			printf("%s\n", argv[1]);
    			break;
    		}
    	}
    
    Thus, the close ;; is equivalent to the close curly brace at the end of the
    "case" block.

--- src/strings/bh_str2hex.sh
+++ src/strings/bh_str2hex.sh
@@ -1,23 +1,23 @@
 bh_str2hex() {
 	(( $# < 1 )) && return 1
 
-	case "$1" in
-		"-x")
+	case $1 in
+		-x)
 			echo -n "$2" | hexdump -ve '/1 "%02x"' | sed 's/../\\x&/g'
 			echo
-			;;
-		"-0x")
+		;;
+		-0x)
 			echo -n "$2" | hexdump -ve '/1 "0x%02x "' | sed 's/\(.*\) /\1/'
 			echo
-			;;
-		"-c")
+		;;
+		-c)
 			echo -n '{ '
 			echo -n "$2" | hexdump -ve '/1 "0x%02x, "' | sed 's/\(.*\), /\1/'
 			echo ' }'
-			;;
-        *)
+		;;
+		*)
 			echo -n "$1" | hexdump -ve '/1 "%02x "' | sed 's/\(.*\) /\1/'
 			echo
-			;;
+		;;
 	esac
 }
