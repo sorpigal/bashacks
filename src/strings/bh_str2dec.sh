90560a73c490e8bf93d73cd76833f5c712d3cabb
sorpigal <sorpigal@gmail.com>
2023-12-01 12:06:59 -0500

    bh_str2dex: avoid external utilities
    A pure-bash rewrite that produces identical output.

--- src/strings/bh_str2dec.sh
+++ src/strings/bh_str2dec.sh
@@ -1,6 +1,10 @@
 bh_str2dec() {
 	(( $# < 1 )) && return 1
 
-	echo -n "$1" | hexdump -ve '/1 "%d "' | sed 's/\(.*\) /\1/'
-	echo
+	local i c ord=()
+	for ((i=0;i<${#1};i++)); do
+		c=${1:i:1}
+		ord+=($(printf '%d\n' "${c/#/\'}"))
+	done
+	printf '%s\n' "${ord[*]}"
 }
