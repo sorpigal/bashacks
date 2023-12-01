8efc46276cff8080c98fa7e5d8f3cc99294c50f5
sorpigal <sorpigal@gmail.com>
2023-12-01 12:09:47 -0500

    bh_str2dec: allow more than one argument
    The ASCII value of each argument will be printed on separate lines.

--- src/strings/bh_str2dec.sh
+++ src/strings/bh_str2dec.sh
@@ -1,10 +1,12 @@
 bh_str2dec() {
-	(( $# < 1 )) && return 1
-
-	local i c ord=()
-	for ((i=0;i<${#1};i++)); do
-		c=${1:i:1}
-		ord+=($(printf '%d\n' "${c/#/\'}"))
+	local i c ord
+	while (( $# )); do
+		ord=()
+		for ((i=0;i<${#1};i++)); do
+			c=${1:i:1}
+			ord+=($(printf '%d\n' "${c/#/\'}"))
+		done
+		printf '%s\n' "${ord[*]}"
+		shift
 	done
-	printf '%s\n' "${ord[*]}"
 }
