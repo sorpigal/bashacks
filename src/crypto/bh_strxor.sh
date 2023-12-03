1bfa46197ada90b433515ded54a6a66349cfb356
sorpigal <sorpigal@gmail.com>
2023-12-03 12:26:49 -0500

    bh_strxor: do not rely wordsplitting from unquoted expansion

--- src/crypto/bh_strxor.sh
+++ src/crypto/bh_strxor.sh
@@ -4,9 +4,9 @@ bh_strxor() {
 	local str i xorged
 
 	# $2 is the string and $1 is the xor key
-	str=$(bh_str2dec "$2")
+	IFS=' ' read -ra str < <(bh_str2dec "$2")
 
-	for i in $str; do
+	for i in "${str[@]}"; do
 		xored=$(( $i ^ $1 ))
 		printf %s "$(bh_dec2asc "$xored")"
 	done
